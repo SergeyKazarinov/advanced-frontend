771f94dfae59280bbd4b01fdaa3ec387ede01a40
Sergey Kazarinov <kazarinov092@gmail.com>
2023-06-08 00:35:18 +0500

    feat(styles): add helpers classNames

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,12 +4,13 @@ import { AboutPageLazy } from "..//pages/AboutPage/AboutPage.lazy";
 import { MainPageLazy } from "../pages/MainPage/MainPage.lazy";
 import { Suspense } from "react";
 import { useTheme } from "..//theme/useTheme";
+import { classNames } from "../helpers/classNames/classNames";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
 
   return ( 
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>Theme</button>
       <Link to={'/about'}>О сайте</Link>
       <Link to={'/'}>Главная</Link>

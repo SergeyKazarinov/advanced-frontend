289b81e0862ee84e7efccffa0c9bcba16993f63e
Sergey Kazarinov <kazarinov092@gmail.com>
2023-06-08 16:25:47 +0500

    refact(ThemeSwitcher): move ThemeSwitcher from navbar to sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { FC, useState } from "react";
 import s from './Sidebar.module.scss';
 import { classNames } from "shared/lib/classNames/classNames";
+import { ThemeSwitcher } from "features/ThemeSwitcher";
 
 interface SidebarProps {
   className?: string; 
@@ -16,6 +17,9 @@ const Sidebar: FC<SidebarProps> = ({className}) => {
   return ( 
     <div className={classNames(s.sidebar, {[s.collapsed]: collapsed}, [className])}>
       <button onClick={onToggle}>asdf</button>
+      <div className={s.switchers}>
+        <ThemeSwitcher />
+      </div>
     </div>
   );
 }

e04e7bfc9d5ba991e7da7afae30fda600e69d65e
Sergey Kazarinov <kazarinov092@gmail.com>
2023-08-10 14:27:55 +0500

    test(cypress): add isolate test

---
+++ cypress/component/EditableProfileCard.cy.tsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line
+import { EditableProfileCard } from "../../src/features/editableProfileCard";
+import { TestProvider } from '../../src/shared/lib/tests/componentRender';
+
+const USER_ID = '1';
+
+describe('EditableProfileCard.cy.tsx', () => {
+  it('playground', () => {
+    cy.intercept('GET', '**/profile/*', { fixture: 'profile.json' });
+    cy.mount(
+      <TestProvider options={{
+        initialState: {
+          user: {
+            authData: {
+              id: USER_ID,
+            },
+          },
+        },
+      }}
+      >
+
+        <EditableProfileCard profileId={USER_ID} />
+      </TestProvider>,
+    );
+  });
+});
--- src/shared/lib/tests/componentRender.tsx
+++ src/shared/lib/tests/componentRender.tsx
@@ -1,29 +1,46 @@
-import { ReducersMapObject } from '@reduxjs/toolkit';
-import { render } from '@testing-library/react';
 import { IStateSchema, StoreProvider } from '@app/providers/StoreProvider';
-import { ReactNode } from 'react';
+// eslint-disable-next-line
+import { ThemeProvider } from '@app/providers/ThemeProvider';
+import { ReducersMapObject } from '@reduxjs/toolkit';
+import i18nForTests from '@shared/config/i18n/i18nForTests';
+import { ThemeEnum } from '@shared/const/theme';
+import { render } from '@testing-library/react';
+import { FC, ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
-import i18nForTests from '@shared/config/i18n/i18nForTests';
+// eslint-disable-next-line
+import '@app/styles/index.scss';
 
 interface componentRenderOptionsProps {
   route?: string;
   initialState?: DeepPartial<IStateSchema>;
-  asyncReducers?: DeepPartial<ReducersMapObject<IStateSchema>>
+  asyncReducers?: DeepPartial<ReducersMapObject<IStateSchema>>;
+  theme?: ThemeEnum;
 }
+
+interface TestProviderProps {
+  children: ReactNode;
+  options?: componentRenderOptionsProps;
+}
+
+export const TestProvider: FC<TestProviderProps> = ({ children, options = {} }) => {
+  const { route = '/', theme = ThemeEnum.DARK } = options;
+  return (
+    <MemoryRouter initialEntries={[route]}>
+      <StoreProvider asyncReducers={options.asyncReducers} initialState={options.initialState as IStateSchema}>
+        <I18nextProvider i18n={i18nForTests}>
+          <ThemeProvider initialTheme={theme}>
+            <div className={`stories ${theme}`}>
+              {children}
+            </div>
+          </ThemeProvider>
+        </I18nextProvider>
+      </StoreProvider>
+    </MemoryRouter>
+  );
+};
 
 export const componentRender = (
   component: ReactNode,
   options: componentRenderOptionsProps = {},
-) => {
-  const { route = '/' } = options;
-  return render(
-    <MemoryRouter initialEntries={[route]}>
-      <StoreProvider asyncReducers={options.asyncReducers} initialState={options.initialState as IStateSchema}>
-        <I18nextProvider i18n={i18nForTests}>
-          {component}
-        </I18nextProvider>
-      </StoreProvider>
-    </MemoryRouter>,
-  );
-};
+) => render(<TestProvider options={options}>{component}</TestProvider>);
